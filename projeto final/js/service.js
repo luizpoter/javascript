5d31570758788d01e50b87bfd5e0fcf63e1d33da
Luiz Felipe <luiz.f.ribeiro10@aluno.senai.br>
2023-12-20 21:25:32 -0300

    upando os arquivos

--- projeto final/js/service.js
+++ projeto final/js/service.js
@@ -1,18 +1,15 @@
 import { handleErrors } from "./exception.js";
+
 var URL = 'http://localhost:3000/ricos';
 
 export const getAllRicos = async() => {
     try {
-        const dataContainer = document.getElementById('data-container');
-        // Fazendo uma solicitação GET para obter produtos da AP
-        const response =
-            await fetch(URL);
+        const response = await fetch(URL);
         //lidando com oerros na resposta
         handleErrors(response);
 
         //converter os dados para json
-        return await response.json();
-
+        return response.json();
 
     } catch (error) {
         console.log('Error >>>', error);
